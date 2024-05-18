4048c5b06729a9bf682aae9c073cb72610689438
MartinUgarte <39602138+MartinUgarte@users.noreply.github.com>
2024-05-18 14:45:41 -0300

    Task1

---
+++ dags.py
@@ -0,0 +1,24 @@
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+default_args = {
+    'owner': 'code2j',
+    'retries': 5,
+    'retry_delay': timedelta(minutes=5), 
+}
+
+with DAG (
+    dag_id='our_first_dag',
+    default_args=default_args,
+    description='This is our first dag that we write',
+    start_date=datetime(2021, 7, 29, 2), # everyday at 2am
+    schedule_interval='@daily', 
+) as dag:
+    task1 = BashOperator(
+        task_id='first_task',
+        bash_command='echo hello world, this is the first task'
+    )
+
+    task1
