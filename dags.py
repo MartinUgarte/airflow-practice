a02ea5761c5ee66c78ac4af28d5bfb4badad352f
MartinUgarte <39602138+MartinUgarte@users.noreply.github.com>
2024-05-18 14:49:23 -0300

    Task3 other methods of dependencies

--- dags.py
+++ dags.py
@@ -10,7 +10,7 @@ default_args = {
 }
 
 with DAG (
-    dag_id='our_first_dag_v3',
+    dag_id='our_first_dag_v4',
     default_args=default_args,
     description='This is our first dag that we write',
     start_date=datetime(2021, 7, 29, 2), # everyday at 2am
@@ -31,5 +31,13 @@ with DAG (
         bash_command='echo hello world, this is the third task and will be runnig after task1 and with the same time as task2'
     )
 
-    task1.set_downstream(task2)
-    task1.set_downstream(task3)
+    # Task Dependencies Method 1
+    #task1.set_downstream(task2)
+    #task1.set_downstream(task3)
+
+    # # Task Dependencies Method 2
+    #task1 >> task2
+    #task1 >> task3
+
+    # Task Dependencies Method 3
+    task1 >> [task2, task3]
