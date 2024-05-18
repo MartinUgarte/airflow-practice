9e43a400334d7181520dbaa638cf855bccaef4ed
MartinUgarte <39602138+MartinUgarte@users.noreply.github.com>
2024-05-18 15:00:22 -0300

    xcoms

--- create_dag_with_python_operator.py
+++ create_dag_with_python_operator.py
@@ -8,12 +8,23 @@ default_args = {
     'retry_delay': timedelta(minutes=5),
 }
 
-def greet(name, age):
-    print(f"Hello World! My name is {name}, and I a {age} years old.")
+def get_name(ti):
+    # return 'Jerry' doesnt work if I want to distinguish more than one returning value
+    ti.xcom_push(key='first_name', value='Jerry')
+    ti.xcom_push(key='last_name', value='Garcia')
+
+def get_age(ti):
+    ti.xcom_push(key='age', value=25)
+
+def greet(ti): # ti = task instance
+    first_name = ti.xcom_pull(task_ids='get_name', key='first_name')
+    last_name = ti.xcom_pull(task_ids='get_name', key='last_name')
+    age = ti.xcom_pull(task_ids='get_age', key='age')
+    print(f"Hello World! My name is {first_name} {last_name}, and I a {age} years old.")
 
 with DAG (
     default_args=default_args,
-    dag_id='our_dag_with_python_operator_v01',
+    dag_id='our_dag_with_python_operator_v6',
     description='This is our first dag that we write with PythonOperator',
     start_date=datetime(2021, 7, 29, 2),
     schedule_interval='@daily',
@@ -21,7 +32,17 @@ with DAG (
     task1 = PythonOperator(
         task_id='greet',
         python_callable=greet,
-        op_kwargs={'name': 'John', 'age': 25}
+        #op_kwargs={'age': 25}
     )
 
-    task1
+    task2 = PythonOperator(
+        task_id='get_name',
+        python_callable=get_name
+    )
+
+    task3 = PythonOperator(
+        task_id='get_age',
+        python_callable=get_age
+    )
+
+    [task2, task3] >> task1
